530482078ebea8c46a54219fb142220dbb18d28e
NanguoBean-surface <Qaqboy@outlook.com>
2022-07-06 16:18:38 +0800

    完成了单周期的控制单元

--- Nanguobean-cpu/Nanguobean-cpu.srcs/sources_1/new/ALU.v
+++ Nanguobean-cpu/Nanguobean-cpu.srcs/sources_1/new/ALU.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-
 module ALU(
     input [3:0] alu_op,
     input [31:0] A,
@@ -9,17 +8,21 @@ module ALU(
     output reg [31:0] res
 );
 
-assign zero = (res == 0)? 1:0;
+assign zero = (res == 0);
 assign less = res[31];
 
 always@(*) begin
     case(alu_op)
-        0: res = A + B;
-        1: res = A - B;
-        2: res = A | B;
-        3: res = A & B;
-        4: res = (A == B)? 1:0;
-        5: res = (A < B)? 1:0;
+        ADD: res = A + B;
+        SUB: res = A + ~B + 1;
+        AND: res = A & B;
+        OR: res = A | B;
+        XOR: res = A ^ B;
+        SLL: res = A << B;
+        SRL: res = A >> B;
+        SRA: res = $unsigned($signed(A) >>> B);
+        SLT: res = (A+~B+1) >= 'h80000000;
+        SLTU: res  = A < B ;
     endcase
 end
 
